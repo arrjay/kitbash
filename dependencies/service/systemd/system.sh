bd6d11716e7f3934db9151bb5567e9d7b6f19a71
RJ Bergeron <rj@bbxn.us>
2026-02-12 19:38:55 -0500

    teach systemd to manually start/stop services too

--- dependencies/service/systemd/system.sh
+++ dependencies/service/systemd/system.sh
@@ -1,6 +1,7 @@
+# shellcheck shell=bash
 # Manage systemd units
 
-system.service.enable() {
+system::service::enable() {
   local _unit=$1; shift
 
   __babashka_log "== ${FUNCNAME[0]} (systemd) $_unit"
@@ -8,7 +9,7 @@ system.service.enable() {
   if systemctl is-enabled "$_unit" 2>&1 | grep -q "No such file or directory" ; then
     __babashka_fail "${FUNCNAME[0]} (systemd): Unit $_unit not installed"
   fi
-  
+
   function get_id() {
     echo "${_unit}"
   }
@@ -18,12 +19,12 @@ system.service.enable() {
     systemctl is-enabled "$_unit" | grep -q "enabled"
   }
   meet() {
-    $__babashka_sudo systemctl enable "$_unit" 2>&1 > /dev/null;
+    $__babashka_sudo systemctl enable "$_unit" > /dev/null 2>&1;
   }
   process
 }
 
-system.service.disable() {
+system::service::disable() {
   local _unit=$1; shift
   __babashka_log "== ${FUNCNAME[0]} (systemd)  $_unit"
 
@@ -31,7 +32,7 @@ system.service.disable() {
   if systemctl is-enabled "$_unit" 2>&1 | grep -q "No such file or directory" ; then
     __babashka_fail "${FUNCNAME[0]} (systemd): Unit $_unit not installed"
   fi
-  
+
   function get_id() {
     echo "${_unit}"
   }
@@ -41,7 +42,51 @@ system.service.disable() {
     systemctl is-enabled "$_unit" | grep -q "disabled"
   }
   meet() {
-    $__babashka_sudo systemctl disable $_unit 2>&1 > /dev/null;
+    $__babashka_sudo systemctl disable "$_unit" > /dev/null 2>&1;
   }
   process
+}
+
+# TODO: what does a failed service look like?
+system::service::started() {
+  local _unit=$1; shift
+
+  __babashka_log "== ${FUNCNAME[0]} (systemd) $_unit"
+  # check if the unit even exists; if it doesn't this makes no sense
+  if systemctl is-enabled "$_unit" 2>&1 | grep -q "No such file or directory" ; then
+    __babashka_fail "${FUNCNAME[0]} (systemd): Unit $_unit not installed"
+  fi
+
+  function get_id() {
+    echo "${_unit}"
+  }
+
+  is_met() {
+    systemctl show --property ActiveState --value "$_unit" | grep -q "active"
+  }
+  meet() {
+    $__babashka_sudo systemctl start "$_unit" > /dev/null 2>&1;
+  }
+  process
+}
+
+system::service::stopped() {
+  local _unit=$1; shift
+
+  __babashka_log "== ${FUNCNAME[0]} (systemd) $_unit"
+  # check if the unit even exists; if it doesn't this makes no sense
+  if systemctl is-enabled "$_unit" 2>&1 | grep -q "No such file or directory" ; then
+    __babashka_fail "${FUNCNAME[0]} (systemd): Unit $_unit not installed"
+  fi
+
+  function get_id() {
+    echo "${_unit}"
+  }
+
+  is_met() {
+    systemctl show --property ActiveState --value "$_unit" | grep -q "inactive"
+  }
+  meet() {
+    $__babashka_sudo systemctl stop "$_unit" > /dev/null 2>&1;
+  }
 }
